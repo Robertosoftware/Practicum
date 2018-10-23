e32be2a95dccf5e2c2a6dc9e2c898ccb6f9b0780
Robertosoftware <42943662+Robertosoftware@users.noreply.github.com>
2018-10-22 23:18:58 -0500

    Update session-admin.php

--- admin/session-admin.php
+++ admin/session-admin.php
@@ -3,11 +3,11 @@
 
   if (!isset($_SESSION['admin_name'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: /admin/login-admin.php');
+  	header('location: Practicum/admin/login-admin.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
   	unset($_SESSION['admin_name']);
-  	header("location: /admin/login-admin.php");
+  	header("location: Practicum/admin/login-admin.php");
   }
 ?>
