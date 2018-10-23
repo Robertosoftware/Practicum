b3468c030981d7f6b1ec5dd0c3d9bddaa4d4aec7
Robertosoftware <42943662+Robertosoftware@users.noreply.github.com>
2018-10-22 23:42:27 -0500

    Update session.php

--- usuario/session.php
+++ usuario/session.php
@@ -3,11 +3,11 @@
 
   if (!isset($_SESSION['correo'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	//header('location: login.php');
+  	header('location: login.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
   	unset($_SESSION['correo']);
-  	//header("location: login.php");
+  	header("location: login.php");
   }
 ?>
