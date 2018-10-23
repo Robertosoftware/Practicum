ec935f6e44399a47fa6eb1217aedd6ce57351c28
Robertosoftware <42943662+Robertosoftware@users.noreply.github.com>
2018-10-22 19:19:10 -0500

    Update server-admin.php

--- server-admin.php
+++ server-admin.php
@@ -16,12 +16,12 @@ if (isset($_POST['login_admin'])) {
   $admin_name = mysqli_real_escape_string($db, $_POST['admin_name']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
-  if (empty($admin_name)) {
-  	array_push($errors, "Nombre requerido para la operación");
-  }
-  if (empty($password)) {
-  	array_push($errors, "Password requerido para la operación");
-  }
+  //if (empty($admin_name)) {
+  	//array_push($errors, "Nombre requerido para la operación");
+  //}
+  //if (empty($password)) {
+  	//array_push($errors, "Password requerido para la operación");
+  //}
 
   if (count($errors) == 0) {
   	$query = "SELECT * FROM admin WHERE admin_name='$admin_name' AND password='$password'";
