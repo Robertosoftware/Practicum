06191d39e6b35593855c75dc424d81ac66dced58
Robertosoftware <42943662+Robertosoftware@users.noreply.github.com>
2018-11-02 18:46:43 -0600

    Update Actualiza.php

--- partials/Actualiza.php
+++ partials/Actualiza.php
@@ -6,10 +6,30 @@
      <div class="jumbotron">
         <h1><?php  if (isset($_SESSION['correo'])) : ?>
     	<p>Hola <strong><?php echo $_SESSION['correo']; ?></strong></p> </h1>
-       <div class="labelac">Email</div>
+       <div class="labelac">Nombre</div>
  <div class="input-group input-group-lg">
  <span class="input-group-addon" id="sizing-addon1"></span>
-  <input type="email" class="form-control" name="correo" ria-describedby="sizing-addon1" value="">
+  <input type="nombre" class="form-control" name="nombre" ria-describedby="sizing-addon1" value="">
+      </div>
+   <div class="labelac">Apellido Paterno</div>
+ <div class="input-group input-group-lg">
+ <span class="input-group-addon" id="sizing-addon1"></span>
+  <input type="nombre" class="form-control" name="apellido_paterno" ria-describedby="sizing-addon1" value="">
+      </div>
+   <div class="labelac">Apellido Materno</div>
+ <div class="input-group input-group-lg">
+ <span class="input-group-addon" id="sizing-addon1"></span>
+  <input type="nombre" class="form-control" name="apellido_materno" ria-describedby="sizing-addon1" value="">
+      </div>
+   <div class="labelac">Edad</div>
+ <div class="input-group input-group-lg">
+ <span class="input-group-addon" id="sizing-addon1"></span>
+  <input type="nombre" class="form-control" name="edad" ria-describedby="sizing-addon1" value="">
+      </div>
+   <div class="labelac">Teléfono</div>
+ <div class="input-group input-group-lg">
+ <span class="input-group-addon" id="sizing-addon1"></span>
+  <input type="nombre" class="form-control" name="telefono" ria-describedby="sizing-addon1" value="">
       </div>
       </div>
     <div class="cuerpo">
