47d9570a4be8c400c45288e2d67df8a4ffe61e84
Robertosoftware <42943662+Robertosoftware@users.noreply.github.com>
2018-11-20 03:32:33 -0600

    Update usuariocompetencia.php

--- partials/usuariocompetencia.php
+++ partials/usuariocompetencia.php
@@ -1,5 +1,5 @@
 <?php include('../usuario/session.php');
-error_reporting(E_ALL ^ E_WARNING);?>
+//error_reporting(E_ALL ^ E_WARNING);?>
 <div class="jumbotron">
                <form method="post" action="usuario/serverusuario.php">
       <?php  if (isset($_SESSION['correo'])) : ?>
@@ -31,41 +31,66 @@ echo $_SESSION['idusuario'];
 <?php
 $qcompetencia1="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
 $rcompetencia1 =mysqli_query($db,$qcompetencia1);
-$rowcompetencia1=mysqli_fetch_array($rcompetencia1);
-$vcompetencia1 = $rowcompetencia1['tecnica_idtecnica'];
-$qcompetencia12="select * from tecnica where idtecnica = $vcompetencia1;";
-$rcompetencia12 =mysqli_query($db, $qcompetencia12);
-$rowcompetencia12=mysqli_fetch_array($rcompetencia12);
-$vcompetencia12 = $rowcompetencia12['nombre'];
+//$rowcompetencia1=mysqli_fetch_array($rcompetencia1);
+//$vcompetencia1 = $rowcompetencia1['tecnica_idtecnica'];
 
- ?>
-
-
-
+                   
+                   $i=0;
+                   $arreglo = array();
+                   $otroarreglo = array();
+                                 $arreglo2 = array();
+                  
+                   
+                    while($res=mysqli_fetch_array($rcompetencia1)){
+                        $i++;
+                        $arreglo2[$i] = $res['tecnica_idtecnica'];
+              
+                           $cons=mysqli_query($db,"select * from tecnica where idtecnica = $arreglo2[$i];");
+                  // $resultado = mysqli_fetch_array($cons);
+                             
+                           
+                                 
+                                 
+    
+                                 while($resultado=mysqli_fetch_array($cons)){
+                                      
+                                     $arreglo[$i] = $resultado['nombre'];
+                                     $otroarreglo[$i] = $resultado['idtecnica'];
+                                
+                                     
+                                 }
+                    }
+                   
+                          ?>
 
                     <div class="labelac">Competencia técnica 1</div>
   <select name="competencia1" required class="form-control dropd">
-    <option value="<?php  echo $vcompetencia1 ?>">
-         <?php  echo $vcompetencia12 ?>
+    <option value="<?php  echo $otroarreglo[1]; ?>">
+         <?php  echo $arreglo[1]; ?>
         </option>
         <?php
+                                 
          $query="select * from tecnica where competencia_idcompetencia='$competencia1'";
         $result= mysqli_query($db, $query);
+                                 
         if($result)
         {
             while($row=mysqli_fetch_array($result)){
                 echo'<option value="'.$row['idtecnica'].'">'.$row['nombre'].'</option>';
             }
         }
+                                 
         ?>
      </select>
-
+<?php
+                                 
+                                     ?>
      <?php
-     $qpericicdad1="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
+     $qpericidad1="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
      $rpericidad1 =mysqli_query($db,$qpericidad1);
      $rowpericidad1=mysqli_fetch_array($rpericidad1);
      $vpericidad1 = $rowpericidad1['pericidad'];
-
+                                 
       ?>
 
 
@@ -82,10 +107,9 @@ $qcompetencia2="select * from usuario_has_tecnica where user_register_id_usuario
 $rcompetencia2 =mysqli_query($db,$qcompetencia2);
 $rowcompetencia2=mysqli_fetch_array($rcompetencia2);
 $vcompetencia2 = $rowcompetencia2['tecnica_idtecnica'];
-$qcompetencia22="select * from tecnica where idtecnica = $vcompetencia2;";
-$rcompetencia22 =mysqli_query($db, $qcompetencia22);
-$rowcompetencia22=mysqli_fetch_array($rcompetencia22);
-$vcompetencia22 = $rowcompetencia22['nombre'];
+         
+         
+         
 
  ?>
 
@@ -93,8 +117,8 @@ $vcompetencia22 = $rowcompetencia22['nombre'];
 
                       <div class="labelac">Competencia técnica 2</div>
   <select name="competencia2" required class="form-control dropd">
-    <option value="<?php  echo $vcompetencia2 ?>">
-        <?php  echo $vcompetencia22 ?>
+    <option value="<?php  echo $otroarreglo[2]; ?>">
+        <?php  echo $arreglo[2]; ?>
         </option>
         <?php
          $query="select * from tecnica where competencia_idcompetencia='$competencia2'";
@@ -109,7 +133,7 @@ $vcompetencia22 = $rowcompetencia22['nombre'];
      </select>
 
      <?php
-     $qpericicdad2="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
+     $qpericidad2="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
      $rpericidad2 =mysqli_query($db,$qpericidad2);
      $rowpericidad2=mysqli_fetch_array($rpericidad2);
      $vpericidad2 = $rowpericidad2['pericidad'];
@@ -126,10 +150,6 @@ $vcompetencia22 = $rowcompetencia22['nombre'];
                    $rcompetencia3 =mysqli_query($db,$qcompetencia3);
                    $rowcompetencia3=mysqli_fetch_array($rcompetencia3);
                    $vcompetencia3 = $rowcompetencia3['tecnica_idtecnica'];
-                   $qcompetencia32="select * from tecnica where idtecnica = $vcompetencia3;";
-                   $rcompetencia32 =mysqli_query($db, $qcompetencia32);
-                   $rowcompetencia32=mysqli_fetch_array($rcompetencia32);
-                   $vcompetencia32 = $rowcompetencia32['nombre'];
 
                     ?>
 
@@ -137,8 +157,8 @@ $vcompetencia22 = $rowcompetencia22['nombre'];
 
         <div class="labelac">Competencia técnica 3</div>
   <select name="competencia3" required class="form-control dropd">
-    <option value="<?php  echo $vcompetencia3 ?>">
-        <?php  echo $vcompetencia32 ?>
+    <option value="<?php  echo $otroarreglo[3]; ?>">
+        <?php  echo $arreglo[3]; ?>
         </option>
         <?php
          $query="select * from tecnica where competencia_idcompetencia='$competencia3'";
@@ -153,21 +173,22 @@ $vcompetencia22 = $rowcompetencia22['nombre'];
      </select>
 
      <?php
-     $qpericicdad3="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
+     $qpericidad3="select * from usuario_has_tecnica where user_register_id_usuario = $id;";
      $rpericidad3 =mysqli_query($db,$qpericidad3);
      $rowpericidad3=mysqli_fetch_array($rpericidad3);
      $vpericidad3 = $rowpericidad3['pericidad'];
-
+                                 
       ?>
 
                 <div class="labelac">Semestres con experiencia</div>
  <div class="input-group input-group-lg">
  <span class="input-group-addon" id="sizing-addon1"></span>
-  <input type="number" class="form-control" name="pericidad3" ria-describedby="sizing-addon1" value="<?php echo $vpericidad2 ?>" required>
+  <input type="number" class="form-control" name="pericidad3" ria-describedby="sizing-addon1" value="<?php echo $vpericidad2; ?>" required>
                    </div>
     <div class="input-group">
     <button type="submit" class="btn btn-success btn-lg" name="usuario_competencia" value="">Siguiente</button>
   	</div>
          </form>
         </div>
-    <?php endif ?>
+    <?php 
+                   endif ?>
